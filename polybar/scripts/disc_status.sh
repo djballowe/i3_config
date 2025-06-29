d704b932806eb447fc711958fdc606308ab888f4
djballowe <djballowe@gmail.com>
2025-06-29 13:43:55 -0700

    script now works for space separated labels

--- polybar/scripts/disc_status.sh
+++ polybar/scripts/disc_status.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-disc_title=$(lsblk -f | grep sr0 | tr -s ' ' ',' | cut -d',' -f4)
+disc_title=$(lsblk -f | grep sr0 | awk '{
+        for (i = 4; i < NF; i++) {
+            printf "%s%s", $i, (i < NF-1 ? " " : "")
+        }
+        printf ""
+}')
 
 if [[ "$disc_title" == "" ]]; then
     echo "NO_DISC"
