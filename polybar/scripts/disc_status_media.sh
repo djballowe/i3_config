ddc9cf5241533e9bd207a2af55472a0b854474ed
djballowe <djballowe@gmail.com>
2026-01-05 22:37:00 -0800

    update disc script

--- polybar/scripts/disc_status_media.sh
+++ polybar/scripts/disc_status_media.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 source ~/.config/i3/script-vars
 
-status=$(mullvad-exclude ssh -i "$SERVER_SSH_KEY_PATH" -p "$SERVER_SSH_PORT" "$SERVER_USER"@"$SERVER_IP" "lsblk -f" | grep sr0 | awk '{
+disc=$(mullvad-exclude ssh -i "$SERVER_SSH_KEY_PATH" -p "$SERVER_SSH_PORT" "$SERVER_USER"@"$SERVER_IP" "lsblk -f" | grep sr0 | awk '{
         for (i = 4; i < NF; i++) {
             printf "%s%s", $i, (i < NF-1 ? " " : "")
         }
         printf ""
 }')
 
-if [[ "$status" == "" ]]; then
-    echo "NO_DISC"
+status=""
+
+if [[ "$disc" == "" ]]; then
+    status="NO_DISC"
 else
-    echo "$status"
+    status="$disc"
 fi
+
+echo ""$FORMAT""$FORMAT_END"  "$status""
